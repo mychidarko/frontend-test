38f131d669960bfd502b0e92498ea875fd00d541
Michael Darko <mickdd22@gmail.com>
2021-12-03 09:13:35 +0000

    add breakpoint and color theme

---
+++ styles/theme/breakpoints.ts
@@ -0,0 +1,9 @@
+const breakpoints = {
+    sm: "@media only screen and (min-width: 576px)",
+    md: "@media only screen and (min-width: 768px)",
+    lg: "@media only screen and (min-width: 992px)",
+    xl: "@media only screen and (min-width: 1200px)",
+    xxl: "@media only screen and (min-width: 1536px)",
+};
+
+export default breakpoints;
