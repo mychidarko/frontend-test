2fd440ea451efbeb28f7f30b81895cb0dd0fef37
Michael Darko <mickdd22@gmail.com>
2021-12-03 09:05:57 +0000

    add content fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { StoreData } from "../@types/data";
+import { useGet } from "../utils/hermes";
 
 const Home: NextPage = () => {
+    const [res, loading, err] = useGet(
+        "https://run.mocky.io/v3/5194b007-b9b9-4748-a63f-f5db0ee4cd47"
+    );
+    const shopData: StoreData = res?.data;
+
+    console.log(res.data);
+
     return (
         <div>
             <Head>
-                <title></title>
-                <meta name="description" content={`Shop for `} />
-                <link rel="icon" href="/favicon.ico" />
+                <title>
+                    {shopData?.hustle?.name || "Hustle Shop"}
+                </title>
+                <meta
+                    name="description"
+                    content={
+                        shopData
+                            ? `Hustle shop for ${shopData.hustle?.name}`
+                            : "Hustle shop"
+                    }
+                />
+                <link
+                    rel="icon"
+                    href={shopData?.hustle?.shop_logo ||  "/favicon.ico"}
+                />
             </Head>
         </div>
     );
